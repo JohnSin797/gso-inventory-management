58a39e7b9728fdbefb1abc7cada87bc69ef5a620
JohnSin797 <jansenangelodolor@gmail.com>
2023-08-23 18:22:19 +0800

    user registration done

--- app/(site)/register/page.js
+++ app/(site)/register/page.js
@@ -1,107 +1,168 @@
 'use client'
 
 import Link from "next/link"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import axios from "axios"
 import Swal from "sweetalert2"
+import PasswordStrengthChecker from "@/app/components/PasswordStrengthChecker"
 
 export default function Register() {
     const router = useRouter()
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [userName, setUserName] = useState("")
-    const [passWord, setPassWord] = useState("")
-    const [usernameError, setUsernameError] = useState("")
-    const [passwordError, setPasswordError] = useState("")
+    const [formData, setFormData] = useState({
+        first_name: '',
+        last_name: '',
+        username: '',
+        password: '',
+      });
+    const [errors, setErrors] = useState({})
     const [strength, setStrength] = useState("");
+    const [isUsernameValid, setIsUsernameValid] = useState(false)
+
+    // const handleStrengthChange = (strength) => {
+    //     setTimeout(() => {
+    //         setStrength(strength)
+    //     }, 500)
+    // }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    // useEffect(() => {
+    //     if (strength == 'weak') {
+    //         setErrors({
+    //             ...errors,
+    //             password: 'Password is too weak. It must be at least medium strength.'
+    //         })
+    //         Swal.fire({
+    //             title: 'Password Error',
+    //             icon: 'error',
+    //             text: 'Password should be alphanumeric and contain at least 1 uppercase character.'
+    //         })
+    //     } else {
+    //         setErrors({
+    //             ...errors,
+    //             password: ''
+    //         })
+    //     }
+    // }, [strength, errors])
+
+    const validateForm = () => {
+        const errors = {};
+
+        validateUsername()
+
+        if (!isUsernameValid) {
+            errors.username = 'Username already exists.';
+        }
+        
+        if (!formData.username.trim()) {
+            errors.username = 'Username is required.';
+        }
+
+        if (!formData.first_name.trim()) {
+            errors.first_name = 'First Name is required.';
+        }
+
+        if (!formData.last_name.trim()) {
+            errors.last_name = 'Last Name is required.';
+        }
+
+        if(strength == 'weak') {
+            errors.password = 'Password is too weak.';
+        }
+
+        if (!formData.password.trim()) {
+            errors.password = 'Password is required.';
+        }
+    
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+      };
 
     const validateUsername = async () => {
-        axios.post('/api/user', {username:userName})
+        axios.post('/api/user', {username:formData.username})
         .then(res=>{
-            setUsernameError(res.data.message)
-            return true
+            setIsUsernameValid(false)
         })
         .catch(err=>{
-            return false
+            setIsUsernameValid(true)
         })
-    }
-
-    const validatePassword = (e) => {
-        setPassWord(e.target.value);
-        const isStrongRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=!])(?=.{10,15})/;
-        const isMediumRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.{8,15})/;
-        if(isStrongRegex.test((passWord))) {
-            setStrength('strong')
-            return false
-        }
-        else if(isMediumRegex.test(passWord)) {
-            setStrength('medium')
-            return false
-        }
-        else {
-            setStrength('weak')
-            return true
-        }
     }
 
     const register = async () => {
-        if(validateUsername()) {}
-        else if(firstName != '' && lastName != '' && userName != '' && passWord != '') {
-            axios.post('/api/user/register', {first_name:firstName,last_name:lastName,username:userName,passWord:passWord})
-            .then(res => {
-                console.log('res', res.data.message);
-                router.push('/')
-            })
-            .catch(err => {
+        setTimeout(()=>{
+            const isFormValid = validateForm()
+            if(errors.password) {
                 Swal.fire({
-                    title: 'Error',
+                    title: 'Password Error',
                     icon: 'error',
-                    text: err.response.data.message
+                    text: 'Password should be alphanumeric and contain at least 1 uppercase character.'
                 })
-            });
-        }
-        else {
-            alert('Please fill in all the fields before you proceed.')
-        }
+            }
+            if(isFormValid) {
+                axios.post('/api/user/register', formData)
+                .then(res => {
+                    console.log('res', res.data.message);
+                    router.push('/')
+                })
+                .catch(err => {
+                    Swal.fire({
+                        title: 'Error',
+                        icon: 'error',
+                        text: err.response.data.message
+                    })
+                });
+            }
+        },1000)
     }
 
     return(
         <div className="absolute flex items-center justify-center h-full w-full">
-            <div className="p-6 border border-cyan-400 rounded md:w-1/4 space-y-2">
-                <p className="text-2xl">General Service Office</p>
+            <div className="p-6 border border-cyan-400 rounded md:w-1/3 space-y-2">
+                <p className="text-2xl text-center">General Service Office</p>
                 <input 
                     type="text" 
                     className="w-full bg-black block border border-cyan-400 placeholder:text-cyan-400"
                     placeholder="First Name"
-                    onChange={(e)=>setFirstName(e.target.value)}
+                    name="first_name"
+                    value={formData.first_name}
+                    onChange={handleChange}
                 />
+                <p className="block h-4 text-red-600 text-xs text-center">{errors.first_name}</p>
                 <input 
                     type="text" 
                     className="w-full bg-black block border border-cyan-400 placeholder:text-cyan-400"
                     placeholder="Last Name"
-                    onChange={(e)=>setLastName(e.target.value)}
+                    name="last_name"
+                    value={formData.last_name}
+                    onChange={handleChange}
                 />
+                <p className="block h-4 text-red-600 text-xs text-center">{errors.last_name}</p>
                 <input 
                     type="text" 
                     className="w-full bg-black block border border-cyan-400 placeholder:text-cyan-400"
                     placeholder="Username"
-                    onChange={(e)=>setUserName(e.target.value)}
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
                 />
-                <p className="block h-4 text-red-600 text-xs text-center">{usernameError}</p>
+                <p className="block h-4 text-red-600 text-xs text-center">{errors.username}</p>
                 <input 
                     type="password" 
                     className="w-full bg-black block border border-cyan-400 placeholder:text-cyan-400"
                     placeholder="Password"
-                    onChange={validatePassword}
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
                 />
-                <div className="flex space-x-1">
-                    <div className={`w-1/3 border ${strength == 'strong' ? 'border-cyan-400' : strength == 'weak' ? 'border-red-600' : strength == 'medium' ? 'border-green-600' : 'border-gray-300'}`}></div>
-                    <div className={`w-1/3 border ${strength == 'strong' ? 'border-cyan-400' : strength == 'medium' ? 'border-green-600' : 'border-gray-300'}`}></div>
-                    <div className={`w-1/3 border ${strength == 'strong' ? 'border-cyan-400' : 'border-gray-300'}`}></div>
-                </div>
-                <p className="block h-3 text-xs text-right text-gray-300">{strength}</p>
-                <p className="block h-4 text-red-600 text-xs text-center">{passwordError}</p>
+                <PasswordStrengthChecker password={formData.password} onStrengthChange={setStrength} />
+                <p className="block h-4 text-red-600 text-xs text-center">{errors.password}</p>
                 <div className="flex justify-between">
                     <button 
                         className="p-1 w-1/2 border-cyan-400 hover:bg-cyan-400 hover:text-black hover:font-bold"

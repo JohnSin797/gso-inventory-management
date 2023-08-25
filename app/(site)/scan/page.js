c7840fba4c9a237aa2bf8bd5ba3b1fe7e4b8e6c7
JohnSin797 <jansenangelodolor@gmail.com>
2023-08-25 22:53:18 +0800

    add new scan page

---
+++ app/(site)/scan/page.js
@@ -0,0 +1,12 @@
+import Navigation from "@/app/components/navigation"    
+
+export default function Scan () {
+    return (
+        <div>
+            <Navigation />
+            <div className="pt-20">
+                Scanner
+            </div>
+        </div>
+    )
+}
--- app/components/navigation.js
+++ app/components/navigation.js
@@ -10,7 +10,8 @@ export default function Navigation() {
         <div className="flex fixed space-x-5 p-6 w-full z-50 bg-black">
             <Link href={"/"} className={`hover:font-bold ${currentPath == '/' ? 'border-b' : ''}`}>Home</Link>
             <Link href={"/department"} className={`hover:font-bold ${currentPath == '/department' ? 'border-b' : ''}`}>Department</Link>
-            <Link href={"/data-entry"} className={`hover:font-bold ${currentPath == '/data-entry' ? 'border-b' : ''}`}>Data Entry</Link>
+            <Link href={"/data-entry"} className={`hover:font-bold ${currentPath.startsWith('/data-entry') ? 'border-b' : ''}`}>Data Entry</Link>
+            <Link href={"/scan"} className={`hover:font-bold ${currentPath == '/scan' ? 'border-b' : ''}`}>Scan</Link>
             <Logout className={'justify-self-end hover:font-bold absolute right-10 text-xs'} />
         </div>
     )

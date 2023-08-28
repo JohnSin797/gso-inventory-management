bc122e7c823fa372b54b54eca84d05f192d2e119
JohnSin797 <jansenangelodolor@gmail.com>
2023-08-29 01:37:14 +0800

    employees page done

--- app/(site)/data-entry/employee/page.js
+++ app/(site)/data-entry/employee/page.js
@@ -1,19 +1,54 @@
+'use client'
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function Employee () {
+
+    const [employees, setEmployees] = useState([])
+
+    const getEmployees = async () => {
+        try {
+            const response = axios.get('/api/employee')
+            setEmployees((await response).data.data)
+            
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    useEffect(()=>{
+        getEmployees()
+    },[])
+
     return (
         <div className="absolute top-60 p-6 flex justify-center items-center w-full">
-            <div className="w-3/5 border rounded p-6 px-20 space-y-2">
-                <form className="space-y-2">
-                    <input 
-                        type="text"
-                        className="w-full bg-black border-b"
-                        placeholder="Department Name"
-                    />
-                    <button
-                        className="p-1 w-1/3 bg-slate-600 hover:bg-cyan-900 rounded"
-                    >
-                        save
-                    </button>
-                </form>
+            <div className="w-3/5 border rounded p-6 space-y-2 h-72 scroll-y">
+                <table className="table-fixed border-separate border border-slate-600 w-full">
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Department</th>
+                            <th>Username</th>
+                            <th>Password</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {console.log(employees)}
+                        {employees.map((item)=>{
+                            return (<tr key={item.id}>
+                                <td className="border border-slate-600">{item.first_name}</td>
+                                <td className="border border-slate-600">{item.last_name}</td>
+                                <td className="border border-slate-600">{item.department}</td>
+                                <td className="border border-slate-600">{item.username}</td>
+                                <td className="border border-slate-600">
+                                    <button className="p-2 w-full text-xs border rounded">get password</button>
+                                </td>
+                            </tr>)
+                        })}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
---
+++ app/api/employee/route.js
@@ -0,0 +1,13 @@
+import connectMongoDB from "@/libs/mongodb";
+import Employee from "@/models/employees";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        await connectMongoDB();
+        const employees = await Employee.find({});
+        return NextResponse.json({message: 'OK', data: employees}, {status: 200});
+    } catch (error) {
+        return NextResponse.json({error: error.message}, {status: 500});
+    }
+}

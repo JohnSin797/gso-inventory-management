3e9425402dc81dbfdc6fd937d14b99eb02a11598
JohnSin797 <jansenangelodolor@gmail.com>
2023-09-08 00:05:14 +0800

    install react-icons

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -5,10 +5,13 @@ import { usePathname } from "next/navigation";
 import Logout from "./logout";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Navigation({ onSetUserRole }) {
     const currentPath = usePathname();
     const [userRole, setUserRole] = useState('')
+    const [isNavHidden, setIsNavHidden] = useState(false)
 
     const getUserRole = async () => {
         await axios.get('/api/user')
@@ -26,12 +29,27 @@ export default function Navigation({ onSetUserRole }) {
     }, [])
 
     return(
-        <div className="flex fixed space-x-5 p-6 w-full z-50 bg-black">
-            <Link href={"/"} className={`hover:font-bold ${currentPath == '/' ? 'border-b' : ''}`}>Home</Link>
-            <Link href={"/department"} className={`hover:font-bold ${currentPath == '/department' ? 'border-b' : ''}`}>Department</Link>
-            <Link href={"/data-entry"} className={`hover:font-bold ${userRole == 'admin' ? '' : 'hidden'} ${currentPath.startsWith('/data-entry') ? 'border-b' : ''}`}>Data Entry</Link>
-            <Link href={"/scan"} className={`hover:font-bold ${currentPath == '/scan' ? 'border-b' : ''}`}>Scan</Link>
-            <Logout className={'justify-self-end hover:font-bold absolute right-10 text-xs'} />
+        <div className={`flex fixed justify-between p-6 w-full z-50 bg-black md:max-h-20 ${isNavHidden ? 'h-20 flex-row md:flex-col' : 'h-full flex-col md:flex-row'}`}>
+            <p className="md:hidden">GSO Inventory Management</p>
+            <div className={`flex flex-col md:flex-row md:space-x-5 ${isNavHidden ? 'hidden md:block' : 'space-y-5 md:space-y-0'}`}>
+                <Link href={"/"} className={`hover:font-bold ${currentPath == '/' ? 'border-b' : ''}`}>Home</Link>
+                <Link href={"/department"} className={`hover:font-bold ${currentPath == '/department' ? 'border-b' : ''}`}>Department</Link>
+                <Link href={"/data-entry"} className={`hover:font-bold ${userRole == 'admin' ? '' : 'hidden'} ${currentPath.startsWith('/data-entry') ? 'border-b' : ''}`}>Data Entry</Link>
+                <Link href={"/scan"} className={`hover:font-bold ${currentPath == '/scan' ? 'border-b' : ''}`}>Scan</Link>
+                <Logout className={'text-left md:text-center md:absolute md:right-5 hover:font-bold md:text-xs'} />
+            </div>
+            <button
+                className={`w-full flex justify-center ${isNavHidden ? 'hidden' : 'md:hidden'}`}
+                onClick={()=>setIsNavHidden(!isNavHidden)}
+            >
+                <AiOutlineClose className="h-8 w-8 text-white" />
+            </button>
+            <button
+                className={`${isNavHidden ? 'md:hidden' : 'hidden'}`}
+                onClick={()=>setIsNavHidden(!isNavHidden)}
+            >
+                <GiHamburgerMenu className="h-8 w-8 text-white" />
+            </button>
         </div>
     )
 }

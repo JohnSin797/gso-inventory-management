dce2f1c723d0492f604141b685ea626c35911960
JohnSin797 <jansenangelodolor@gmail.com>
2023-09-25 23:16:04 +0800

    temp fix

--- middleware.js
+++ middleware.js
@@ -1,28 +1,29 @@
 import { NextResponse } from 'next/server'
  
 export function middleware(request) {
-    // const path = request.nextUrl.pathname
+    const path = request.nextUrl.pathname
 
-    // const isPublicPath = path == '/login'
+    const isPublicPath = path == '/signin'
 
-    // const token = request.cookies.get('token')?.value || ''
+    const token = request.cookies.get('token')?.value || ''
 
-    // if(isPublicPath && token) {
-    //     return NextResponse.redirect(new URL('/', request.nextUrl))
-    // }
+    if(isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
 
-    // if(!isPublicPath && !token) {
-    //     return NextResponse.redirect(new URL('/login', request.nextUrl))
-    // }
+    if(!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/signin', request.nextUrl))
+    }
 }
 
 export const config = {
     matcher: [
         '/',
-        '/login',
+        '/user',
+        '/signin',
         '/department',
         '/employee',
         '/data-entry/:path*',
-        '/scan'
+        '/inventory'
     ],
 }
